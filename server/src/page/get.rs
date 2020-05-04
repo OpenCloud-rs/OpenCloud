0d76a446e2d5cfa98895d4b9a4ff7b8a1b95a46d
Rheydskey <matgag02@gmail.com>
2020-05-04 22:38:28 +0200

    Working hard on download

--- server/src/page/get.rs
+++ server/src/page/get.rs
@@ -1,8 +1,9 @@
+use crate::lib::http::{last_cli, without_cli};
+use actix_files::file_extension_to_mime;
 use actix_web::dev::BodyEncoding;
 use actix_web::http::ContentEncoding;
 use actix_web::{web, Error, HttpRequest, HttpResponse as Response};
 use futures::StreamExt;
-use crate::lib::http::last_cli;
 
 pub async fn cli(req: HttpRequest, mut body: web::Payload) -> Result<Response, Error> {
     crate::lib::http::log(&req);
@@ -16,23 +17,32 @@ pub async fn cli(req: HttpRequest, mut body: web::Payload) -> Result<Response, E
         let item = item?;
         match String::from_utf8(item.to_vec()) {
             Ok(o) => {
-                let vec: Vec<&str> = o.split('=').collect();
+                let vec: Vec<&str> = o.split(|c| c == '=' || c == '&').collect();
+                println!("{:?}", vec);
                 if vec[0] == "type" {
                     match vec[1] {
                         "download" => {
-                            result = Ok(Response::Ok()
-                                .header("Access-Control-Allow-Origin", "*")
-                                .header("charset", "utf-8")
-                                .content_type(
-                                    format!("Content-Disposition: attachment; filename={}", last_cli(req.clone())),
-                                )
-                                .encoding(ContentEncoding::Gzip).body("Hey"));
-                            /*result = Ok(Response::Ok()
-                                .header("Access-Control-Allow-Origin", "*")
-                                .header("charset", "utf-8")
-                                .content_type("application/json")
-                                .encoding(ContentEncoding::Gzip)
-                                .body(crate::lib::file::dir_content(&req)));*/
+                            if vec[3] == "zip" {
+                                result = Ok(Response::Ok()
+                                    .header("Access-Control-Allow-Origin", "*")
+                                    .header("charset", "utf-8")
+                                    .header("Content-Disposition", "attachment")
+                                    .header("filename", format!("{}{}",last_cli(req.clone()), ".zip"))
+                                    .content_type(
+                                        file_extension_to_mime(without_cli(req.clone().path()))
+                                            .essence_str(),
+                                    )
+                                    .encoding(ContentEncoding::Gzip)
+                                    .body("dd"));
+                            } else {
+                                result = Ok(Response::Ok()
+                                    .header("Access-Control-Allow-Origin", "*")
+                                    .header("charset", "utf-8")
+                                    .header("Content-Disposition", "attachment")
+                                    .header("filename", format!("{}{}",last_cli(req.clone()), ".tar.gz"))
+                                    .content_type(file_extension_to_mime(without_cli(req.clone().path())).essence_str())
+                                    .encoding(ContentEncoding::Gzip).body("dd"));
+                            }
                         }
 
                         _ => {
