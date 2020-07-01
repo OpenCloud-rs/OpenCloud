6f808d940c249d7774a7491099216b6c9107566a
Matteo Zarola <matzar1808@gmail.com>
2020-07-01 16:26:47 +0200

    Better install script

--- install.sh
+++ install.sh
@@ -1,5 +1,24 @@
-git clone https://github.com/Rheydskey/OpenCloud.git
-cd OpenCloud
-cargo install --force cargo-make
-cargo make build
-cargo make start_release
+#!/bin/bash
+#git clone https://github.com/Rheydskey/OpenCloud.git
+#cd OpenCloud
+#cargo install --force cargo-make
+#cargo make build
+#cargo make start_release
+if [ -f "./Makefile.toml" ]; then 
+    OPENCLOUD_DIR = $PWD
+else 
+    git clone https://github.com/Rheydskey/OpenCloud.git opencloud 
+    cd opencloud 
+    OPENCLOUD_DIR = $PWD
+fi
+cd "${OPENCLOUD_DIR}" 
+if [ command -v "cargo" ]; then 
+    echo "Cargo install found !"
+elif 
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > rustup.sh 
+    echo "Installing cargo..."
+    bash rustup.sh -y 
+    source $HOME/.cargo/env
+fi
+cargo install cargo-make
+cargo make build_release
