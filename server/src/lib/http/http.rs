12afd4aae432585c393da8b8c7a078f977acb755
Rheydskey <matgag02@gmail.com>
2020-08-27 20:21:18 +0200

    Add get_args function

--- server/src/lib/http/http.rs
+++ server/src/lib/http/http.rs
@@ -1,4 +1,5 @@
 use actix_web::HttpRequest;
+use std::collections::BTreeMap;
 
 pub fn without_api(string: &str) -> &str {
     string
@@ -24,4 +25,24 @@ pub fn last_cli(req: HttpRequest) -> String {
         result = String::from(split.last().take().unwrap().to_owned())
     }
     result
+}
+
+pub fn get_args(req: HttpRequest) -> BTreeMap<String, String> {
+    let mut bvec: BTreeMap<String, String> = BTreeMap::new();
+    let vec: Vec<&str> = req.query_string().split(|c| c == '&').collect();
+    for i in 0..vec.len() {
+        if let Some(_u) = vec[i].rfind("=") {
+            let e: Vec<&str> = vec[i].split("=").collect();
+            if e[0].is_empty() {
+                continue;
+            }
+            bvec.insert(e[0].to_string(), e[1].to_string());
+            continue;
+        }
+        if vec[i].is_empty() {
+            continue;
+        }
+        bvec.insert(vec[i].to_string(), String::from(""));
+    }
+    bvec
 }
--- server/src/page/get.rs
+++ server/src/page/get.rs
@@ -1,41 +1,26 @@
 use std::collections::BTreeMap;
 use actix_web::{HttpRequest, HttpResponse as Response};
 use crate::lib::file::file::{get_dir};
-use crate::lib::archive::archive::*;
+use crate::lib::{http::http::get_args, archive::archive::*};
 use actix_http::body::Body;
 
 pub async fn cli(req: HttpRequest) -> std::io::Result<Response<Body>> {
     println!("{:?} ---",req.query_string());
     let result;
 
-    let mut bvec: BTreeMap<&str, &str> = BTreeMap::new();
-    let vec: Vec<&str> = req.query_string().split(|c| c == '&').collect();
+    let bvec = get_args(req.clone());
 
-    for i in 0..vec.len() {
-        if let Some(_u) = vec[i].rfind("=") {
-            let e: Vec<&str> = vec[i].split("=").collect();
-            if e[0].is_empty() {
-                continue;
-            }
-            bvec.insert(e[0], e[1]);
-            continue;
-        }
-        if vec[i].is_empty() {
-            continue;
-        }
-        bvec.insert(vec[i], &"");
-    }
     if bvec.contains_key("download") {
         match bvec.get("download").unwrap().as_ref() {
             "tar.gz" => {
-                result = get_tar(&req).await;
+                result = get_tar(req.clone()).await;
             }
             _ => {
-                result = get_zip(&req).await;
+                result = get_zip(req.clone()).await;
             }
         }
     } else {
-        result = get_dir(&req);
+        result = get_dir(req.clone());
     }
     result
 }
