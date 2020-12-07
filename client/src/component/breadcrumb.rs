8ee2bf3a5be4d66df67054141ada9d297dca0745
Rheydskey <matgag02@gmail.com>
2020-12-07 21:33:52 +0100

    :adhesive_bandage: Add a "/" for home in the breadcrumb

--- client/src/component/breadcrumb.rs
+++ client/src/component/breadcrumb.rs
@@ -3,13 +3,18 @@ use seed::{prelude::*, *};
 
 pub fn breadcrumb(url: String) -> Node<Msg> {
     let vec: Vec<&str> = url.split("/").collect();
+
     nav![
         C!["breadcrumb is-centered bd-snippet notification has-text-black has-background-link-light"],
             ul![
+                li![
+                    a![
+                        format![""]
+                      ]
+                    ],
                 vec.iter().map( |t|
-                                    li![
+                    li![
                         a![
-                            attrs![At::Href => format!["{}", t]],
                             format!["{}", t]
                           ]
                         ]
