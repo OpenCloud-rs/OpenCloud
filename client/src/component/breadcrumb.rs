082a4821f2c4758c5e647bf752db0da50346f490
Rheydskey <matgag02@gmail.com>
2020-12-08 11:07:08 +0100

    :bug: Fix double slash on breadcrumb

--- client/src/component/breadcrumb.rs
+++ client/src/component/breadcrumb.rs
@@ -3,22 +3,27 @@ use seed::{prelude::*, *};
 
 pub fn breadcrumb(url: String) -> Node<Msg> {
     let vec: Vec<&str> = url.split("/").collect();
-
+    let mut n = 0;
     nav![
         C!["breadcrumb is-centered bd-snippet notification has-text-black has-background-link-light"],
             ul![
                 li![
-                    a![
-                        format![""]
-                      ]
+                        format!{""}
                     ],
-                vec.iter().map( |t|
+                vec.iter().map( |t| if t.is_empty() {
+                    if n == 0 {
+                        li![""]    
+                    } else {
+                        empty![]
+                    }
+                } else {
+                    n+= 1;
                     li![
                         a![
-                            format!["{}", t]
+                            format!{"{}", t}
                           ]
                         ]
-                    )
+                })
                 ]
         ]
 }
