4b99923fddcff5c6066c3d532a0af28b417d9ed8
Rheydskey <33085093+Rheydskey@users.noreply.github.com>
2021-01-08 23:27:39 +0100

    :recycle: Refactor some codes

--- server/src/lib/db/user/get.rs
+++ server/src/lib/db/user/get.rs
@@ -6,7 +6,6 @@ use crate::lib::db::user::hash_password;
 
 pub async fn get_users() -> Vec<User> {
     let mut conn = conn().await;
-    let mut vec: Vec<User> = Vec::new();
     let mut response = sqlx::query("SELECT * FROM User").fetch(&mut conn);
     let mut person_vec: Vec<User> = Vec::new();
     while let Ok(Some(row)) = response.try_next().await {
@@ -16,15 +15,11 @@ pub async fn get_users() -> Vec<User> {
             password: row.try_get("password").expect("Error"),
             token: row.try_get("token").unwrap_or(String::new()),
             email: row.try_get("email").unwrap_or(String::new()),
-            home: String::new(),
+            home: format!("./home/{}/",row.try_get("name").expect("Error")),
         });
     }
 
-    for mut person in person_vec {
-        person.home = format!("./home/{}/", person.name);
-        vec.push(person);
-    }
-    vec
+    person_vec
 }
 pub async fn get_user_by_token(token: String) -> Option<User> {
     let mut conn = conn().await;
@@ -39,18 +34,11 @@ pub async fn get_user_by_token(token: String) -> Option<User> {
             password: row.try_get(2).expect("Error"),
             token: row.try_get(3).unwrap_or(String::new()),
             email: row.try_get(4).unwrap_or(String::new()),
-            home: String::new(),
+            home: format!("./home/{}", row.try_get(1).expect("Error")),
         });
     }
-
-    let mut result = None;
-    for mut user in user_vec {
-        user.home = format!("./home/{}", user.name);
-        result = Some(user);
-        break;
-    }
-
-    result
+    
+    user_vec.first();
 }
 
 pub async fn get_id_of_user(name: String, password: String) -> Option<i32> {
