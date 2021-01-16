d5d22d973f75e28e1eac6278b7f5351d8ab96baa
Rheydskey <matgag02@gmail.com>
2021-01-16 23:34:02 +0100

    :rocket: Change request to reqwest

--- client/src/http/get/get_files.rs
+++ client/src/http/get/get_files.rs
@@ -1,28 +1,23 @@
-use crate::Msg;
+use crate::{http::get_ip, Msg};
 use seed::log;
-use seed::prelude::*;
 use shared::JsonStruct;
 pub async fn get_files(from: String, token: String) -> Msg {
-    let request = Request::new(format!("http://127.0.0.1:8081/api/file/{}", from))
-    .method(Method::Get)
-    .header(Header::custom("Token", token.as_str()))
-    .fetch()
-    .await;
+    let reqwest = reqwest::Client::new()
+        .get(format!("{}/api/file/{}", get_ip(), from).as_str())
+        .header("Token", token.clone())
+        .send()
+        .await;
 
-    let json = match request {
-        Ok(e) =>{
-            match e.json::<JsonStruct>().await {
-                Ok(json) => {
-                    json
-                },
-                Err(e) =>  {
-                    log!(format!{"{:?}", e});
-                    JsonStruct::new()
-                }
+    let json = match reqwest {
+        Ok(e) => match e.json::<JsonStruct>().await {
+            Ok(json) => json,
+            Err(e) => {
+                log!(format! {"{:?}", e});
+                JsonStruct::new()
             }
         },
         Err(e) => {
-            log!(format!{"{:?}", e});
+            log!(format! {"{:?}", e});
             JsonStruct::new()
         }
     };
